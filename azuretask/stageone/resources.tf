5590408d627db9690ec0d4bfbc0bdc6b67a5f6d8
Akalugo David <109105332+rigamortus@users.noreply.github.com>
2023-11-20 19:53:27 +0100

    Update resources.tf

--- azuretask/stageone/resources.tf
+++ azuretask/stageone/resources.tf
@@ -118,6 +118,18 @@ resource "azurerm_network_security_group" "my-security" {
   }
 
   security_rule {
+    name                       = "httpsaccess"
+    priority                   = 110
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "443"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
     name                       = "test"
     priority                   = 120
     direction                  = "Inbound"
