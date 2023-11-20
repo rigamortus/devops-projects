92d5322f44baeecd446cc5b15924031eca60a748
Akalugo David <109105332+rigamortus@users.noreply.github.com>
2023-11-20 11:11:28 +0100

    Update provider.tf

--- azuretask/stagethree/provider.tf
+++ azuretask/stagethree/provider.tf
@@ -20,14 +20,18 @@ terraform {
       version = "~> 2.0"
     }
   }
+  backend "azurerm" {
+    resource_group_name  = "my-f23-rg"
+    storage_account_name = "tfstatemy12"
+    container_name       = "mytfgithub"
+    key                  = "terraform.tfstate"
+  }
 }
-
 provider "azurerm" {
   #skip_provider_registration = true # This is only required when the User, Service Principal, or Identity running Terraform lacks the permissions to register Azure Resource Providers.
   features {}
+  client_id       = "c9d0477d-c277-401a-9a23-fd3644ebc3e7"
+  client_secret   = var.client_secret
+  tenant_id       = "03024f44-e3ad-4b2c-b85b-d1a744b0b70f"
+  subscription_id = "6ea6f9f7-0a28-45a1-b63d-c045d73026f2"
 }
-
-provider "acme" {
-  server_url = "https://acme-v02.api.letsencrypt.org/directory"
-}
-
