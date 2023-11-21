c5fdb293db99d380bb90c15f3461e52cc07a8e1d
Akalugo David <109105332+rigamortus@users.noreply.github.com>
2023-11-21 12:02:31 +0100

    Update data.tf

--- azuretask/stagethree/data.tf
+++ azuretask/stagethree/data.tf
@@ -42,9 +42,10 @@ data "azurerm_network_security_group" "my-security" {
 
 data "azurerm_shared_image_version" "example" {
   name                = "0.0.1"
-  image_name          = "my-image"
+  image_name          = "my-template"
   gallery_name        = "my_gallery"
   resource_group_name = "my-f22-rg"
+  sort_versions_by_semver = true
 }
 
 data "azurerm_application_gateway" "agw" {
