969dbf223812a55bf8cde2a24bf4da73eab803fb
Akalugo David <109105332+rigamortus@users.noreply.github.com>
2023-11-22 08:37:36 +0100

    Delete azuretask/webserver.ps1

--- azuretask/webserver.ps1
+++
@@ -1,6 +0,0 @@
-C:\Windows\system32\sysprep\sysprep.exe /generalize /oobe /shutdown /mode:vm -Wait
-
-#openssl pkcs12 -export -out yourdomain.pfx -inkey /etc/letsencrypt/live/yourdomain.com/privkey.pem -in /etc/letsencrypt/live/yourdomain.com/cert.pem -certfile /etc/letsencrypt/live/yourdomain.com/chain.pem
-#openssl rsa -in myapp01.key -outform PEM -out myapp01.pem
-
-#openssl pkcs12 -export -out davidcloud.pfx -inkey myapp01.key -in myapp01.crt
