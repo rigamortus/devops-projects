2ab541be0071af04e0792f9f843022c9bfdf4e5f
Akalugo David <109105332+rigamortus@users.noreply.github.com>
2023-11-23 09:30:44 +0100

    Update data.tf

--- azuretask/stageone/data.tf
+++ azuretask/stageone/data.tf
@@ -1,17 +1,4 @@
 data "azurerm_client_config" "current" {
 }
 
-data "azurerm_shared_image_version" "example" {
-  name                = "0.0.1"
-  image_name          = "my-template"
-  gallery_name        = "my_gallery"
-  resource_group_name = "my-f22-rg"
-  sort_versions_by_semver = true
-}
-
-data "azurerm_image" "myimage" {
-  name                = "my-image"
-  resource_group_name = "my-f22-rg"
-}
-
 
