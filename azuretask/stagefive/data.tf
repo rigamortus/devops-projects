9dd36bd224acd1864117c7d214a9ba96e1d731fe
Akalugo David <109105332+rigamortus@users.noreply.github.com>
2023-11-23 09:18:44 +0100

    Create data.tf

---
+++ azuretask/stagefive/data.tf
@@ -0,0 +1,15 @@
+data "azurerm_shared_image_version" "example" {
+  name                = "0.0.1"
+  image_name          = "my-template"
+  gallery_name        = "my_gallery"
+  resource_group_name = "my-f22-rg"
+  sort_versions_by_semver = true
+}
+
+data "azurerm_image" "myimage" {
+  name                = "my-image"
+  resource_group_name = "my-f22-rg"
+}
+
+data "azurerm_client_config" "current" {
+}
